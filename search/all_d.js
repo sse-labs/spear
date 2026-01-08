52db01718f2cc73df77e2306b10d4cbcae60a725
printerboi <printerboi@users.noreply.github.com>
2026-01-08 15:30:51 +0000

    Deploying to gh-pages from @ sse-labs/spear@4a73c241f19ca8bdf7b2edba5ce144705e4d793f 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -8,7 +8,7 @@ var searchData=
   ['printdotrepresentation_5',['printDotRepresentation',['../classProgramGraph.html#afbe3107b10cdec049c59c5c5c804ca87',1,'ProgramGraph']]],
   ['printedges_6',['printEdges',['../classProgramGraph.html#a9e653f7294600e06d970989638aa3d59',1,'ProgramGraph']]],
   ['printnodes_7',['printNodes',['../classProgramGraph.html#a31499bd68418b43bda45e0d76799744b',1,'ProgramGraph']]],
-  ['printpreorder_8',['printpreorder',['../classLoopTree.html#ad97586e2ef41e7fa8f720ee920b3db34',1,'LoopTree::printPreOrder()'],['../classFunctionTree.html#ad338d9921dc8a5f50788079eb8c4b508',1,'FunctionTree::printPreorder()']]],
+  ['printpreorder_8',['printpreorder',['../classFunctionTree.html#ad338d9921dc8a5f50788079eb8c4b508',1,'FunctionTree::printPreorder()'],['../classLoopTree.html#ad97586e2ef41e7fa8f720ee920b3db34',1,'LoopTree::printPreOrder()']]],
   ['profile_9',['profile',['../classDRAMProfiler.html#a6f5497ffd9b6dee2417f57000072f272',1,'DRAMProfiler::profile()'],['../classCPUProfiler.html#ac6db47ff8d35ee789ed3703c96b14097',1,'CPUProfiler::profile()'],['../classMetaProfiler.html#a9bf3701e464313e7d4fa9c5f99ba4a69',1,'MetaProfiler::profile()'],['../classProfiler.html#af50d7dfce1868917936dec58632a0284',1,'Profiler::profile()']]],
   ['profilehandler_10',['profilehandler',['../classProfileHandler.html',1,'ProfileHandler'],['../classProfileHandler.html#a327cc7868566e947376546ac04d77ad2',1,'ProfileHandler::ProfileHandler()']]],
   ['profileoptions_11',['ProfileOptions',['../classProfileOptions.html',1,'']]],
