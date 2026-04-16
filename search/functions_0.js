98b95339639ec73be3be3384f9f5e3f4976c52b2
printerboi <32494292+printerboi@users.noreply.github.com>
2026-04-16 21:48:02 +0000

    Deploying to gh-pages from @ sse-labs/spear@fed61fe042ecfe4266d5b827f359b8eb882f99eb 🚀

--- search/functions_0.js
+++ search/functions_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['_5fmean_0',['_mean',['../classCPUProfiler.html#a13a2b6b5e195ee90099dce26c2111fee',1,'CPUProfiler']]],
   ['_5fmedian_1',['_median',['../classCPUProfiler.html#adb33d464c5c84c54e18c06db472d4ec8',1,'CPUProfiler']]],
-  ['_5fregression_2',['_regression',['../classCPUProfiler.html#a6d0611f4d60f7d1fde12b54e1130a702',1,'CPUProfiler']]]
+  ['_5fregression_2',['_regression',['../classCPUProfiler.html#a9c1a868916e4f8bf125c8d1f9720f2f1',1,'CPUProfiler']]]
 ];
